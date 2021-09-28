aaa09a4e3ccb150e431e4a1d9079d1e777f35886
manojnathyogi <85493350+manojnathyogi@users.noreply.github.com>
2021-09-28 13:39:42 +0545

    Add files via upload

---
+++ final_project/server.py
@@ -0,0 +1,29 @@
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    translated_text = translator.english_to_french(textToTranslate)
+    # return "Translated text to French"
+    return "Translated text to French: " + translated_text
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    # Write your code here
+    translated_text = translator.french_to_english(textToTranslate)
+    # returns "Translated text to English"
+    return "Translated text to English: " + translated_text
+
+@app.route("/")
+def renderIndexPage():
+    # Write the code to render template
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
